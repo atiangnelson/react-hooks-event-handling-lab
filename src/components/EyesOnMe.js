b3d36fade61730a32f82e5550a57539ed4db52d4
Nelson <nelson.atiang@student.moringaschool.com>
2025-04-16 17:48:29 +0300

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+    const handleBlur=()=>{
+        return console.log('Hey! Eyes on me!')
+
+    }
+    const handleFocus=()=>{
+        return console.log('Good!');
+        
+    }
+  return (
+    <div>
+        <buton onBlur={handleBlur} onFocus={handleFocus}>Eyes on me </buton>
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,21 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const keys=[
+        '1','2','3',
+        '4','5','6',
+        '7','8','9',
+        '*','0','#'
+    ]
     return (
-        <div></div>
+        <div className="keypad">
+            <input type="password" onChange={()=>{
+                return console.log("Entering password...")
+            }}/>
+            {keys.map((key)=>{
+                return <button key={key}>{key}</button>
+            })}
+        </div>
     )
 }
 
